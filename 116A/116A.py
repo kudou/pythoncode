9249f2d80d214499e81c81e5e5afe39952351808
kudou <hwp4869@163.com>
2013-05-25 04:28:51 +0800

    DEC diagnostics would run on a dead whale. Added missing 's' clause in input case condition

--- 116A/116A.py
+++
@@ -1,9 +0,0 @@
-total = input()
-result = 0
-result_list = []
-for index in range(total):
-    out_number, in_number = map(int, raw_input().split())
-    result += in_number - out_number 
-    result_list.append(result)
-    
-print sorted(result_list)[-1] 
